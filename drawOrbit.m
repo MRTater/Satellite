9fc5f3bf1453845a6887bba09dcfa6fda6689acf
ZTREvenstar <68584478+ZTREvenstar@users.noreply.github.com>
2020-07-24 22:50:38 +0800

    Update drawOrbit.m

--- drawOrbit.m
+++ drawOrbit.m
@@ -1,11 +1,12 @@
 function drawOrbit(orbits,num_of_orbit)
 %This function draws all satellite orbits in the same figure of the earth
+%first we derive the parameter of the circle, then use plot3 function.
 
 for i = 1 : num_of_orbit
     r = orbits(i).height + 6371;
     normalVector = [sin(orbits(i).polar) * cos(orbits(i).azim) sin(orbits(i).azim) * sin(orbits(i).polar) cos(orbits(i).polar)];
     c = [0 0 0];
-    theta = (0:2 * pi / 100 : 2 * pi);
+    theta = (0:2 * pi / 100 : 2 * pi); % parameter
     a = cross(normalVector,[1 0 0]);
     if ~any(a)
       a = cross(normalVector,[0 1 0]);
